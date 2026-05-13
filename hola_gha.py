75a8f8d28c743db3057a8d421935982984b0d7ba
Anali Madai Osorio Ramirez <amadys1377@gmail.com>
2026-05-12 20:30:36 -0600

    Workflow creado

--- hola_gha.py
+++ hola_gha.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     name = os.getenv("USERNAME")
-    print(f"Hola {name}! Estoy en Github Actions")
+    print(f"Hola {name}! Estoy en Github Actions!git add")
 
 
 if __name__ == "__main__":
